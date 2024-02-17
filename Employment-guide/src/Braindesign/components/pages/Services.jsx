f9b3c331789fd2766ec106cca920baa4977ef718
Nyelus101 <cmuonyelu@gmail.com>
2024-02-17 16:38:55 +0100

    Modal function

--- Employment-guide/src/Braindesign/components/pages/Services.jsx
+++ Employment-guide/src/Braindesign/components/pages/Services.jsx
@@ -17,20 +17,107 @@ export default function Services() {
 
 
 
+//THE MAIN!!!
+// import React from 'react';
+// import '../../App.css';
+// import Exam from '../../Quiz-app/Exam.jsx';
+// // import QuizApp from '../../../Test/QuizApp.jsx';
+// import "../../../Test/index.scss";
 
-import React from 'react';
-import '../../App.css';
-//import Exam from '../../Quiz-app/Exam.jsx';
+// export default function Services() {
+//   return (
+//     <>
+//       {/* <QuizApp /> */}
+//       <Exam />
+      
+//     </>
+//   );
+// }
+
+/*<h1 className='services'>SERVICES</h1>*/
+
+
+//THE MAIN MODAL
+// import { useState } from 'react';
+// import Button from 'react-bootstrap/Button';
+// import Modal from 'react-bootstrap/Modal';
+// // import Exam from '../../Quiz-app/Exam.jsx';
+// import QuizApp from '../../../Test/QuizApp.jsx';
+// import "../../../Test/index.scss";
+
+// function Services() {
+//   const [show, setShow] = useState(false);
+
+//   const handleClose = () => setShow(false);
+//   const handleShow = () => setShow(true);
+
+//   return (
+//     <>
+//       <Button variant="primary" onClick={handleShow}>
+//         Launch static backdrop modal
+//       </Button>
+
+//       <Modal
+//         show={show}
+//         onHide={handleClose}
+//         backdrop="static"
+//         keyboard={false}
+//       >
+//         <Modal.Header closeButton>
+//           <Modal.Title>Modal title</Modal.Title>
+//         </Modal.Header>
+//         <Modal.Body>
+//           <QuizApp />
+//         </Modal.Body>
+//         <Modal.Footer>
+//           <Button variant="secondary" onClick={handleClose}>
+//             Close
+//           </Button>
+//           <Button variant="primary">Understood</Button>
+//         </Modal.Footer>
+//       </Modal>
+//     </>
+//   );
+// }
+
+// export default Services;
+
+
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Exam from '../../Quiz-app/Exam.jsx';
 import QuizApp from '../../../Test/QuizApp.jsx';
 import "../../../Test/index.scss";
 
-export default function Services() {
+function Services() {
+  const values = [true];
+  const [fullscreen, setFullscreen] = useState(true);
+  const [show, setShow] = useState(false);
+
+  function handleShow(breakpoint) {
+    setFullscreen(breakpoint);
+    setShow(true);
+  }
+
   return (
     <>
-      <QuizApp />
-      
+      {values.map((v, idx) => (
+        <Button key={idx} className="me-2 mb-2" onClick={() => handleShow(v)}>
+          Click to Start Quiz
+          {typeof v === 'string' && `below ${v.split('-')[0]}`}
+        </Button>
+      ))}
+      <Modal show={show} fullscreen={fullscreen} onHide={() => setShow(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>THE SECP QUIZ</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <QuizApp />
+        </Modal.Body>
+      </Modal>
     </>
   );
 }
 
-/*<h1 className='services'>SERVICES</h1>*/
+export default Services;
