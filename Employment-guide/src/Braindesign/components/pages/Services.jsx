8988d77617e521c42c495d3d2be3f58e33835c1b
Nyelus101 <cmuonyelu@gmail.com>
2024-02-19 03:35:25 +0100

    update

--- Employment-guide/src/Braindesign/components/pages/Services.jsx
+++ Employment-guide/src/Braindesign/components/pages/Services.jsx
@@ -1,3 +1,47 @@
+
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Exam from '../../Quiz-app/Exam.jsx';
+import QuizApp from '../../../Test/QuizApp.jsx';
+import "../../../Test/index.scss";
+
+function Services() {
+  const values = [true];
+  const [fullscreen, setFullscreen] = useState(true);
+  const [show, setShow] = useState(false);
+
+  function handleShow(breakpoint) {
+    setFullscreen(breakpoint);
+    setShow(true);
+  }
+
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      {values.map((v, idx) => (
+        <Button key={idx} className="me-2 mb-2" onClick={() => handleShow(v)}>
+          Click to Start Quiz
+          {typeof v === 'string' && `below ${v.split('-')[0]}`}
+        </Button>
+      ))}
+      <Modal show={show} fullscreen={fullscreen} onHide={() => setShow(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>THE SECP QUIZ</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Exam />
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
+}
+
+export default Services;
+
+
+
+
+
 /*import React from 'react';
 import '../../App.css';
 
@@ -82,42 +126,3 @@ export default function Services() {
 
 // export default Services;
 
-
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import Exam from '../../Quiz-app/Exam.jsx';
-import QuizApp from '../../../Test/QuizApp.jsx';
-import "../../../Test/index.scss";
-
-function Services() {
-  const values = [true];
-  const [fullscreen, setFullscreen] = useState(true);
-  const [show, setShow] = useState(false);
-
-  function handleShow(breakpoint) {
-    setFullscreen(breakpoint);
-    setShow(true);
-  }
-
-  return (
-    <div className="d-flex justify-content-center align-items-center vh-100">
-      {values.map((v, idx) => (
-        <Button key={idx} className="me-2 mb-2" onClick={() => handleShow(v)}>
-          Click to Start Quiz
-          {typeof v === 'string' && `below ${v.split('-')[0]}`}
-        </Button>
-      ))}
-      <Modal show={show} fullscreen={fullscreen} onHide={() => setShow(false)}>
-        <Modal.Header closeButton>
-          <Modal.Title>THE SECP QUIZ</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Exam />
-        </Modal.Body>
-      </Modal>
-    </div>
-  );
-}
-
-export default Services;
